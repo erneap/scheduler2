06a79a53d3ee98febad4fc6b6bf17d67c00deceb
Anton Erne <antonerne@yahoo.com>
2023-10-25 04:44:34 +0900

    Oct25-0444

--- scheduler-client/src/app/employee/employee-contact-info/employee-contact-info-item/employee-contact-info-item.component.ts
+++ scheduler-client/src/app/employee/employee-contact-info/employee-contact-info-item/employee-contact-info-item.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Employee, IEmployee } from 'src/app/models/employees/employee';
+import { ContactType } from 'src/app/models/teams/contacttype';
+import { AuthService } from 'src/app/services/auth.service';
+import { DialogService } from 'src/app/services/dialog-service.service';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-contact-info-item',
@@ -6,5 +11,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-contact-info-item.component.scss']
 })
 export class EmployeeContactInfoItemComponent {
+  private _contactType: ContactType = new ContactType();
+  @Input()
+  public set contacttype(ct: ContactType) {
+    this._contactType = new ContactType(ct);
+  }
+  get contacttype(): ContactType {
+    return this._contactType;
+  }
+  private _employee: Employee = new Employee();
+  @Input()
+  public set employee(emp: IEmployee) {
+    this._employee = new Employee(emp);
+  }
+  get employee(): Employee {
+    return this._employee;
+  }
+  contact: string = '';
+  contactid: number = 0;
 
+  constructor(
+    protected empService: EmployeeService,
+    protected dialogService: DialogService,
+    protected authService: AuthService
+  ) {
+
+  }
+
+  setContactType() {
+    this.contact = '';
+    this.employee.contactinfo.forEach(c => {
+      if (c.typeid === this.contacttype.id) {
+        this.contactid = c.id;
+        this.contact = c.value;
+      }
+    });
+  }
+
+  updateContactType() {
+    
+  }
 }

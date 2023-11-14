7d9bed95c266edf8d7a47fee9b5d83551a31e18f
Anton Erne <antonerne@yahoo.com>
2023-11-14 17:22:59 +0900

    Nov14-1722

--- multiview-client/src/app/site/site-employees/site-employee/site-employee-leave-balance/site-employee-leave-balance-dialog/site-employee-leave-balance-dialog.component.ts
+++ multiview-client/src/app/site/site-employees/site-employee/site-employee-leave-balance/site-employee-leave-balance-dialog/site-employee-leave-balance-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-site-employee-leave-balance-dialog',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./site-employee-leave-balance-dialog.component.scss']
 })
 export class SiteEmployeeLeaveBalanceDialogComponent {
+  balanceForm: FormGroup;
 
+  constructor(
+    public dialogRef: MatDialogRef<SiteEmployeeLeaveBalanceDialogComponent>,
+    private fb: FormBuilder
+  ) {
+    const now = new Date();
+    this.balanceForm = this.fb.group({
+      year: [now.getFullYear(), [Validators.required]],
+    });
+  }
+
+  onCancel() {
+    this.dialogRef.close('');
+  }
+
+  onOk() {
+    this.dialogRef.close(`${this.balanceForm.value.year}`);
+  }
 }

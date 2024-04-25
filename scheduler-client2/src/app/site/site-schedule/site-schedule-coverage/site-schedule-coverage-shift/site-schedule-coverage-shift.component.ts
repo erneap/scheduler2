c4186165beb96c290c5e207540e5028b1aed4671
Anton Erne <antonerne@yahoo.com>
2024-04-25 12:37:00 +0900

    Apr25-1236

--- scheduler-client2/src/app/site/site-schedule/site-schedule-coverage/site-schedule-coverage-shift/site-schedule-coverage-shift.component.ts
+++ scheduler-client2/src/app/site/site-schedule/site-schedule-coverage/site-schedule-coverage-shift/site-schedule-coverage-shift.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ISite, Site } from 'src/app/models/sites/site';
+import { Shift } from 'src/app/models/sites/workcenter';
 
 @Component({
   selector: 'app-site-schedule-coverage-shift',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./site-schedule-coverage-shift.component.scss']
 })
 export class SiteScheduleCoverageShiftComponent {
-
+  private _site: Site = new Site();
+  @Input()
+  public set site(s: ISite) {
+    this._site = new Site(s);
+  }
+  get site(): Site {
+    return this._site;
+  }
+  private _dates: Date[] = [];
+  @Input()
+  public set dates(dt: Date[]) {
+    this._dates = [];
+    dt.forEach(d => {
+      this._dates.push(new Date(d));
+    })
+  }
+  get dates(): Date[] {
+    return this._dates
+  }
+  @Input() wkctrID: string = '';
+  @Input() shift: Shift = new Shift();
+  @Input() viewtype: string = 'label';
+  @Input() width: number = 25;
 }
